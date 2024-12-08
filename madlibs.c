f889ac7ea5cfbcd84a470e847e1d743b7a53e18c
Kyle Kennedy <kylekennedy@unr.edu>
2024-12-08 00:40:49 +0000

    kyle 4

--- madlibs.c
+++ madlibs.c
@@ -2,86 +2,59 @@
 //Final Project
 
 #include <stdio.h>
-#define FILE_NAME "madlib1.txt"
+#define FILE_NAME "madlib2.txt"
 #define MAX_LENGTH 200
 #define MAX_ROWS 200
 
-void print_file();
 void file_store(int*num_rows,char row[MAX_ROWS][MAX_LENGTH]);
 void stringCopy(char[], char[]);
 void stringSwap(char[], char[]);
+void condition_A(char adj[], char row[], int *countA);
+void condition_N(char non[], char row[], int *countN);
+void condition_V(char ver[], char row[], int *countV);
+int length(char str[]);
 //fucntion to pull from document(scan)
 //function to display madlib
 int main(){
 	int  a, v, n, j, num_rows, countA=0, countN=0, countV=0;
-	char row[MAX_ROWS][MAX_LENGTH], i, adjective[MAX_ROWS][MAX_LENGTH], noun[MAX_ROWS], verb[MAX_ROWS], adj[MAX_ROWS][MAX_LENGTH], ver[MAX_ROWS][MAX_LENGTH], non[MAX_ROWS][MAX_LENGTH];
-	
+	char row[MAX_ROWS][MAX_LENGTH], adj[MAX_ROWS][MAX_LENGTH], ver[MAX_ROWS][MAX_LENGTH], non[MAX_ROWS][MAX_LENGTH];
 	file_store(&num_rows, row);
 	for(j=0; j<num_rows;j++){
-		
 		if(row[j][0] ==  'A'){
-			printf("please enter an adjective: ");
-			scanf("%s", adj[countA]);
-			stringSwap(adj[countA],row[j]);
-			countA++;
+			condition_A(adj[countA],row[j], &countA);
 		}
 		else if(row[j][0] ==  'N'){
-			printf("please enter an noun: ");
-			scanf("%s", &non[countN]);
-			stringSwap(non[countN],row[j]);
-			countN++;
+			condition_N(non[countN],row[j], &countN);
 		}
 		else if(row[j][0] ==  'V'){
-			printf("please enter an verb: ");
-			scanf("%s", &ver[countV]);
-			stringSwap(ver[countV],row[j]);
-			countV++;
+			condition_V(ver[countV],row[j], &countV);
 		}
-//		else{
-//			printf("%s", row[j]);
-//		}
 	}
-	
-	printf("countA: %d, countN: %d, countV: %d\n" , countA, countN, countV);
 	for(j=0; j<num_rows;j++){
-		printf("%s ", row[j]);
+		printf("%s", row[j]);
 	}
 	printf("\n");
 	return 0;
 }
-
 void file_store(int*num_rows,char row[MAX_ROWS][MAX_LENGTH]){
-
-	
 	FILE*fptr;
 	fptr= fopen(FILE_NAME, "r");
-
 	if(fptr==NULL){
 		printf("nope!\n");
 	}
 	*num_rows=0;
 	while(*num_rows<MAX_ROWS && fgets(row[*num_rows], MAX_LENGTH, fptr) !=NULL){
-		
+		int length=0;
+		while(row[*num_rows][length] != '\0'){
+			if(row[*num_rows][length] == '\n'){
+				row[*num_rows][length]= ' ';
+			}
+			length++;
+		}
 		(*num_rows)++;
 	}
 	fclose(fptr);
-	
-	printf("file_store\n");
-
 }
-void print_file(){
-	int i;
-	FILE*fptr;
-	fptr= fopen(FILE_NAME, "r");
-	if(fptr==NULL){
-		printf("nope!\n");
-	}
-	while(fscanf(fptr, "%c", &i)==1){
-		printf("%c", i);
-	}
-	fclose(fptr);
-	printf("print_file\n");
-	}
 void stringCopy(char source[], char dest[]) {
     int index = 0;
     while (source[index] != '\0') {
@@ -95,4 +68,37 @@ void stringSwap(char str1[], char str2[]) {
     stringCopy(str1, temp);
     stringCopy(str2, str1);
     stringCopy(temp, str2); 
+}
+void condition_A(char adj[], char row[], int *countA){
+	printf("please enter an adjective: ");
+	scanf("%s", adj);
+	int len= length(adj);
+	adj[len]= ' ';
+	adj[len+1]= '\0';
+	stringSwap(adj,row);
+	*countA++;
+}
+void condition_N(char non[], char row[], int *countN){
+	printf("please enter an noun: ");
+	scanf("%s", non);
+	int len= length(non);
+	non[len]= ' ';
+	non[len+1]= '\0';
+	stringSwap(non,row);
+	*countN++;
+}
+void condition_V(char ver[], char row[], int *countV){
+	printf("please enter an verb: ");
+	scanf("%s", ver);
+	int len= length(ver);
+	ver[len]= ' ';
+	ver[len+1]= '\0';
+	stringSwap(ver,row);
+	*countV++;
+}
+int length(char str[]){
+	int length=0;
+	while(str[length] != '\0')
+		length++;
+	return length;
 }
